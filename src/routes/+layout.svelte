2f736085e14afe4a9c646527496222a5e5578672
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-01 11:50:48 +0200

    Update app to run on Vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,9 +6,9 @@
     import Sidebar from '$lib/components/Sidebar.svelte';
     import Header from '$lib/components/Header.svelte';
     import Footer from '$lib/components/Footer.svelte';
-    import TableOfContents from '$lib/components/TableOfContents.svelte';
-    import { browser } from '$app/environment';
-    import { page } from '$app/stores';
+    import { dev } from '$app/environment';
+    import { inject } from '@vercel/analytics';
+    inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="relative flex">
