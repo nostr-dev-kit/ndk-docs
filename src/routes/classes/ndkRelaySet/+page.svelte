7af89ad8afc253880a5f28cc291c08ca5771934c
alltheseas <64376233+alltheseas@users.noreply.github.com>
2023-05-09 14:41:33 -0500

    Adding skeleton classs pages

---
+++ src/routes/classes/ndkRelaySet/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+    import TableOfContents from '$lib/components/TableOfContents.svelte';
+    import MarkdownContent from './ndkRelaySet.svx';
+
+    let tableOfContents: App.NavLink[] = [
+        {
+            title: 'Properties',
+            url: '#properties',
+            sublinks: [
+                { title: 'cacheAdapter', url: '#cacheAdapter' },
+                { title: 'debug', url: '#debug' },
+                { title: 'explicitRelayUrls', url: '#explicitRelayUrls' },
+                { title: 'pool', url: '#pool' },
+                { title: 'signer', url: '#signer' }
+            ]
+        },
+        {
+            title: 'Methods',
+            url: '#methods',
+            sublinks: [
+                { title: 'constructor', url: '#constructor' },
+                { title: 'assertSigner', url: '#assertSigner' },
+                { title: 'connect', url: '#connect' },
+                { title: 'fetchEvent', url: '#fetchEvent' },
+                { title: 'fetchEvents', url: '#fetchEvents' },
+                { title: 'getUser', url: '#getUser' },
+                { title: 'publish', url: '#publish' },
+                { title: 'subscribe', url: '#subscribe' }
+            ]
+        }
+    ];
+</script>
+
+<div class="contentWrapper">
+    <MarkdownContent />
+</div>
+
+<TableOfContents links={tableOfContents} />
