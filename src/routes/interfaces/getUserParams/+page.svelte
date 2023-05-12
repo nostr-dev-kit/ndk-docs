4c42fc24fce65b91dad414d901d5f7035d6c76f7
Jeff Gardner <202880+erskingardner@users.noreply.github.com>
2023-05-12 17:50:22 +0200

    Add getUserParams

---
+++ src/routes/interfaces/getUserParams/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import TableOfContents from '$lib/components/TableOfContents.svelte';
+    import MarkdownContent from './getUserParams.svx';
+
+    let tableOfContents: App.NavLink[] = [
+        {
+            title: 'Properties',
+            url: '#properties',
+            sublinks: [
+                { title: 'hexpubkey', url: '#hexpubkey' },
+                { title: 'nip05', url: '#nip05' },
+                { title: 'npub', url: '#npub' },
+                { title: 'relayUrls', url: '#relayUrls' }
+            ]
+        }
+    ];
+</script>
+
+<div class="contentWrapper">
+    <MarkdownContent />
+</div>
+
+<TableOfContents links={tableOfContents} />
